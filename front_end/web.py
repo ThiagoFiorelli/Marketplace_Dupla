efcfe92094d1240f123232cad9629bf78165fe4d
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-04 15:58:40 -0300

    Merge agora sim correto

--- front_end/web.py
+++ front_end/web.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-<<<<<<< HEAD
 titulo_head = 'Lojinha'
-=======
-
-<<<<<<< HEAD
-titulo_head = 'Lojinha'
-
-=======
-<<<<<<< HEAD
->>>>>>> main
->>>>>>> 5ee3ead864261045519271c9f05f31326612b822
 
 @app.route('/cadastrar_marketplace')
 def cadastro_Marketplace():
@@ -31,29 +21,11 @@ def cadastro_Produto():
         product_price = request.args.get('price')
         create_product(product_name, product_descrpition, product_price)
         menssagem = f'{product_name} cadastrado com sucesso'
-
-<<<<<<< HEAD
     return render_template('create_product.html', menssagem=menssagem)
-=======
+
 @app.route('/')
 def home():
     return render_template('home.html', titulo_head='titulo_head')
 
 
 app.run(debug=True)
-<<<<<<< HEAD
-=======
-=======
-titulo_head = 'Lojinha'
->>>>>>> 5ee3ead864261045519271c9f05f31326612b822
-
-@app.route('/')
-def home():
-    return render_template('home.html', titulo_head = 'titulo_head')
-
-app.run(debug=True)
-<<<<<<< HEAD
-=======
->>>>>>> main
->>>>>>> main
->>>>>>> 5ee3ead864261045519271c9f05f31326612b822
