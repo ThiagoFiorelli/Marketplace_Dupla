0c00fa1a53d402eb3fc32ae6f41f2030053abb19
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-04 15:09:11 -0300

    Criacao de estruturas back-end para cadastro de produto e aicao na base de dados.

--- back_end/actions.py
+++ back_end/actions.py
@@ -1,5 +1,9 @@
 def create_marketplace():
     pass
 
-def create_product():
+    db.add_marketplace(marketplace)
+    marketplace = f'{name};{description}'
+def create_marketplace(name: str, description: str):
+import back_end.db as db
+from back_end.log import generate_log
     pass
--- front_end/web.py
+++ front_end/web.py
@@ -2,4 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+@app.route('/cadastrar_marketplace')
+def cadastro_Marketplace():
+    menssagem = ''
+    marketplace_add = request.args.get('market')
+    if marketplace_add is not None:
+
+        menssagem = f'{marketplace_add} cadastrado com sucesso'
+    return render_template('create_marketplace.html', menssagem=menssagem)
+
+
 app.run(debug=True)
