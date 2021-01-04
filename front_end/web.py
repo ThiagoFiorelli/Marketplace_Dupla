41e0216f3c3d8fc24b980820d4defa7f0fe4f99f
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-04 15:14:18 -0300

    Add home template.

--- front_end/web.py
+++ front_end/web.py
@@ -2,4 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+titulo_head = 'Lojinha'
+
+@app.route('/')
+def home():
+    return render_template('home.html', titulo_head = 'titulo_head')
+
 app.run(debug=True)
