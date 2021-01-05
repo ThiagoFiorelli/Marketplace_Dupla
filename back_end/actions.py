620f54c1c3ca88479dbd1bd2b8f80d4995d9ec75
Juliana Karoline <julianaklulo@gmail.com>
2021-01-05 13:18:45 -0300

    Conserta \n nas strings

--- back_end/actions.py
+++ back_end/actions.py
@@ -6,16 +6,15 @@ from back_end.log import generate_log
 import back_end.db as db
 
 def create_marketplace(name: str, description: str):
-    marketplace = f'{name};{description}\n'
+    marketplace = f'{name};{description}'
     db.add_marketplace(marketplace)
     generate_log(f'Adicionado o marketplace "{name}" ao database.')
 
 
 def create_product(name: str, description: str, price: float):
-    product = f'{name};{description};{price}\n'
+    product = f'{name};{description};{price}'
     db.add_product(product)
     generate_log(f'Adicionado o produto "{name}" ao database.')
-    pass
 
 
 def verify():
--- back_end/db.py
+++ back_end/db.py
@@ -9,7 +9,7 @@ product_db = 'data/product.txt'
 
 def add_product(product: str) -> None:
     with open(product_db, 'a', encoding='utf-8') as products_file:
-        products_file.write(product)
+        products_file.write(f'{product}\n')
 
 
 def verify_db():
@@ -19,4 +19,4 @@ def verify_db():
 
 def add_marketplace(marketplace: str) -> None:
     with open(marketplaces_txt, 'a', encoding='utf-8') as marketplaces_file:
-        marketplaces_file.write(marketplace)
+        marketplaces_file.write(f'{marketplace}\n')
