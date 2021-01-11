d1d6c35cda0ad69920efd5f3c062828b67a1a282
Ralph <ralph.souza@olist.com>
2021-01-11 13:42:28 +0000

    Refatoração de código para marketplaces e sellers.

--- back_end/actions.py
+++ back_end/actions.py
@@ -7,29 +7,6 @@ import back_end.controller.ctrl_category as db
 import back_end.controller.ctrl_product as db_prod
 
 
-
-def create_marketplace(name: str, description: str):
-    marketplace = f'{name};{description}'
-    db.add_marketplace(marketplace)
-    generate_log(f'Cadastro do marketplace "{name}" ao database.')
-
-def create_seller(name: str, phone: str, email: str):
-    seller = f'{name};{phone};{email}'
-    db.add_seller(seller)
-    generate_log(f'Cadastro do seller "{name}" ao database.')
-
-
-def list_marketplaces():
-    marketplaces = db.read_marketplace()
-    generate_log('Listado todos os marketplaces.')
-    return marketplaces
-
-
-def list_sellers():
-    sellers = db.read_seller()
-    generate_log('Listado todos os sellers.')
-    return sellers
-
 #pode ser tirado do codigo
 def list_logs():
     logs = read_logs()
