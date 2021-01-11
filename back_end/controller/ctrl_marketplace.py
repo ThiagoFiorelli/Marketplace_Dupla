42dcf624c5e894f0b5a7d0aafb7eee45039617f9
Ralph <ralph.souza@olist.com>
2021-01-11 13:38:08 +0000

    Refatoração de código com criação de controller para marketplace.

---
+++ back_end/controller/ctrl_marketplace.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('.')
+
+import back_end.dao.dao_txt as dao_ac_txt
+import back_end.dao.dao_log as dao_ac_log
+
+def create_marketplace(name: str, description: str):
+    marketplace = f'{name};{description}'
+    dao_ac_txt.add_marketplace(marketplace)
+    dao_ac_log.generate_log(f'Cadastro do marketplace "{name}" ao database.')
+
+
+def list_marketplaces():
+    marketplaces = dao_ac_txt.read_marketplace()
+    dao_ac_log.generate_log('Listado todos os marketplaces.')
+    return marketplaces
