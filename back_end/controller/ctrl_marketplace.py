c5da956a69a124d082c5ce7e6e9d49950c0560e3
Ralph <ralph.souza@olist.com>
2021-01-11 18:38:04 +0000

    Refatoração de código e estruturas SQL.

--- back_end/controller/ctrl_marketplace.py
+++ back_end/controller/ctrl_marketplace.py
@@ -1,11 +1,12 @@
 import sys
 sys.path.append('.')
 
-import back_end.dao.dao_txt_marketplace as dao_ac_txt
+import back_end.dao.dao_sql_marketplace as dao_ac_txt
 import back_end.dao.dao_log as dao_ac_log
 
 def create_marketplace(name: str, description: str):
     marketplace = f'{name};{description}'
+    print(marketplace)
     dao_ac_txt.add_marketplace(marketplace)
     dao_ac_log.generate_log(f'Cadastro do marketplace "{name}" ao database.')
 
