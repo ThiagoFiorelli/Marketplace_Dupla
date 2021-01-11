b15133c9a4cb2a84b656001209e0ca92835f1216
Ralph <ralph.souza@olist.com>
2021-01-11 13:41:10 +0000

    Refatoração de funções para criação e listagem de marketplaces e sellers.

--- back_end/dao/dao_txt.py
+++ back_end/dao/dao_txt.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.append('/Markeplace_Dupla/back_end/')
+
 product_db = 'data/product.txt'
 categories_db = 'data/categories.txt'
+marketplaces_txt = '../marketplaces.txt'
+sellers_db = '../sellers.txt'
 
 #nao achamos a chamda no codigo, talvez possa ser tirado
 def verify_db():
@@ -34,3 +39,31 @@ def read_categories() -> list:
     return categories
 
 
+def add_marketplace(marketplace: str) -> None:
+    with open(marketplaces_txt, 'a', encoding='utf-8') as marketplaces_file:
+        marketplaces_file.write(f'{marketplace}\n')
+
+
+def read_marketplace() -> list:
+    with open(marketplaces_txt, 'r', encoding='utf-8') as marketplaces_file:
+        marketplaces = {}
+        for mkt in marketplaces_file:
+            name, description = mkt.strip().split(';')
+            marketplaces[name] = description
+
+    return marketplaces
+
+
+def add_seller(seller: str) -> None:
+    with open(sellers_db, 'a', encoding='utf-8') as sellers_file:
+        sellers_file.write(f'{seller}\n')
+
+
+def read_seller() -> list:
+    with open(sellers_db, 'r', encoding='utf-8') as sellers_file:
+        sellers = []
+        for seller in sellers_file:
+            name, phone, email = seller.strip().split(';')
+            sellers.append({'name': name, 'phone': phone, 'email': email})
+
+    return sellers
