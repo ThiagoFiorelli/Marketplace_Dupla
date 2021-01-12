86edc20d06035194a6835d14384ef7121dfe2594
Regina Beretta <reginabmre@gmail.com>
2021-01-12 15:22:06 -0400

    Model Category e Marketplace, refatoração para atendê-los

--- back_end/controller/ctrl_marketplace.py
+++ back_end/controller/ctrl_marketplace.py
@@ -1,17 +1,15 @@
 import sys
 sys.path.append('.')
 
-import back_end.dao.dao_sql_marketplace as dao_ac_txt
+import back_end.dao.dao_sql_marketplace as dao_ac
 import back_end.controller.ctrl_log as ac_log
+from ..model.Marketplace import Marketplace
 
-def create_marketplace(name: str, description: str):
-    marketplace = f'{name};{description}'
-    print(marketplace)
-    dao_ac_txt.add_marketplace(marketplace)
-    ac_log.save_log(f'Cadastro do marketplace "{name}" ao database.')
+def create_marketplace(mkp:Marketplace)->None:
+    dao_ac.add_marketplace(mkp)
+    ac_log.save_log(f'Cadastro do marketplace "{mkp.name}" ao database.')
 
-
-def list_marketplaces():
-    marketplaces = dao_ac_txt.read_marketplace()
+def list_marketplaces()->list:
+    marketplaces = dao_ac.read_marketplace()
     ac_log.save_log('Listado todos os marketplaces.')
     return marketplaces
--- back_end/dao/dao_txt_marketplace.py
+++ back_end/dao/dao_txt_marketplace.py
@@ -1,16 +1,16 @@
+from ..model.Marketplace import Marketplace
+
 marketplaces_txt = 'data/marketplaces.txt'
 
-
-def add_marketplace(marketplace: str) -> None:
+def add_marketplace(marketplace: Marketplace) -> None:
     with open(marketplaces_txt, 'a', encoding='utf-8') as marketplaces_file:
         marketplaces_file.write(f'{marketplace}\n')
 
-
 def read_marketplace() -> list:
     with open(marketplaces_txt, 'r', encoding='utf-8') as marketplaces_file:
-        marketplaces = {}
+        marketplaces = []
         for mkt in marketplaces_file:
             name, description = mkt.strip().split(';')
-            marketplaces[name] = description
-
+            mkplace = Marketplace(name, description)
+            marketplaces.append(mkplace)
     return marketplaces
