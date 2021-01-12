ed867a2953d4e5a4c6fda6a50d61231852561a72
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-12 15:41:08 -0300

    Criação de produtos reestruturada

--- back_end/controller/ctrl_product.py
+++ back_end/controller/ctrl_product.py
@@ -3,11 +3,11 @@ sys.path.append('.')
 
 import back_end.dao.dao_sql_product as dao_ac_txt
 import back_end.controller.ctrl_log as ac_log
+from back_end.models.product import Product
 
-def create_product(name: str, description: str, price: float):
-    product = f'{name};{description};{price}'
+def create_product(product: Product):
     dao_ac_txt.add_product(product)
-    ac_log.save_log(f'Cadastro do produto "{name}" ao database.')
+    ac_log.save_log(f'Cadastro do produto "{product.get_name()}" ao database.')
 
 def list_products():
     products =  dao_ac_txt.read_products()
