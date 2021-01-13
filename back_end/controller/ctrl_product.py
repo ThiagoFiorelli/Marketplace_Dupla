3b494ba0e2819c886c3055eecb6ad81c1fff66f8
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-13 11:01:25 -0300

    Alteração em apelidos produto e seller

--- back_end/controller/ctrl_product.py
+++ back_end/controller/ctrl_product.py
@@ -1,15 +1,15 @@
 import sys
 sys.path.append('.')
 
-import back_end.dao.dao_sql_product as dao_ac_txt
+import back_end.dao.dao_sql_product as dao_ac
 import back_end.controller.ctrl_log as ac_log
 from back_end.models.product import Product
 
 def create_product(product: Product):
-    dao_ac_txt.add_product(product)
+    dao_ac.add_product(product)
     ac_log.create_log(f'Cadastro do produto "{product.get_name()}" ao database.')
 
 def list_products():
-    products = dao_ac_txt.read_products()
+    products = dao_ac.read_products()
     ac_log.create_log('Listado todos os produtos.')
     return products
--- back_end/controller/ctrl_seller.py
+++ back_end/controller/ctrl_seller.py
@@ -1,16 +1,16 @@
 import sys
 sys.path.append('.')
 
-import back_end.dao.dao_sql_seller as dao_ac_txt
+import back_end.dao.dao_sql_seller as dao_ac
 import back_end.controller.ctrl_log as ac_log
 from back_end.models.seller import Seller
 
 def create_seller(seller: Seller):
-    dao_ac_txt.add_seller(seller)
+    dao_ac.add_seller(seller)
     ac_log.create_log(f'Cadastro do seller "{seller.get_name()}" ao database.')
 
 
 def list_sellers():
-    sellers = dao_ac_txt.read_sellers()
+    sellers = dao_ac.read_sellers()
     ac_log.create_log('Listado todos os sellers.')
     return sellers
