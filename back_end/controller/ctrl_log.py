6b145a1ac26a6b871882277240d45e4d0f7f89e0
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-13 10:36:30 -0300

    Nome função controller modificado

--- back_end/controller/ctrl_log.py
+++ back_end/controller/ctrl_log.py
@@ -5,17 +5,13 @@ sys.path.append('.')
 import back_end.dao.dao_sql_log as dao_log
 from back_end.models.log import Log
 
-
-def save_log(dado:str):
+def create_log(dado:str):
     hora_atual= datetime.now().strftime('%H:%M:%S')
     data_atual= datetime.now().strftime('%d/%m/%Y')
-    log = Log(hora_atual, data_atual, dado)
+    log= Log(hora_atual, data_atual, dado)
     dao_log.generate_log(log)
 
-
-
 def list_log() -> list:
-    list_aux=[]
-    list_aux = dao_log.read_logs()
+    list_aux= []
+    list_aux= dao_log.read_logs()
     return list_aux
-
