ba7a331ed86d3ab8b875a4dfbe4dd4fa583362ad
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-18 13:24:12 -0300

    Criação de setters produto

--- back_end/models/product.py
+++ back_end/models/product.py
@@ -5,23 +5,35 @@ class Product:
     __price: float
 
     def __init__(self, name: str, description: str, price: float, identifier: int = None):
-        self.__identifier = identifier
-        self.__name = name
-        self.__description = description
-        self.__price = price
+        self.identifier = identifier
+        self.name = name
+        self.description = description
+        self.price = price
     
     @property
     def identifier(self) -> int:
         return self.__identifier
+    @identifier.setter
+    def identifier(self, identifier: int) -> None:
+        self.__identifier = identifier
 
     @property
     def name(self) -> str:
         return self.__name
+    @name.setter
+    def name(self, name: str) -> None:
+        self.__name = name
 
     @property
     def description(self) -> str:
         return self.__description
-    
+    @description.setter
+    def description(self, description: str) -> None:
+        self.__description = description    
+
     @property
     def price(self) -> float:
         return self.__price
+    @price.setter
+    def price(self, price: float) -> None:
+        self.__price = price
