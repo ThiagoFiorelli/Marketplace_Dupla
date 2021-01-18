fe18c174c194f83958e18b88dba9fab6f54091d7
Clara Baumgarten <clara.odebrecht@olist.com>
2021-01-18 13:21:04 -0300

    Criação de setters log

--- back_end/models/log.py
+++ back_end/models/log.py
@@ -8,26 +8,38 @@ class Log:
 
     def __init__(self, message: str, hour: str = None, date: str = None, identifier: int = None):
         if hour == None:
-            self.__hour = datetime.now().strftime('%H:%M:%S')
-            self.__date = datetime.now().strftime('%d:%m:%Y')
+            self.hour = datetime.now().strftime('%H:%M:%S')
+            self.date = datetime.now().strftime('%d:%m:%Y')
         else:
-            self.__hour = hour
-            self.__date = date
-        self.__message = message
-        self.__identifier = identifier
+            self.hour = hour
+            self.date = date
+        self.message = message
+        self.identifier = identifier
     
     @property
     def identifier(self) -> int:
         return self.__identifier
+    @identifier.setter
+    def identifier(self, identifier: str) -> None:
+        self.__identifier = identifier
     
     @property
     def hour(self) -> str:
         return self.__hour
+    @hour.setter
+    def hour(self, hour: str) -> None:
+        self.__hour = hour
     
     @property
     def date(self) -> str:
         return self.__date
+    @date.setter
+    def date(self, date: str) -> None:
+        self.__date = date
     
     @property
     def message(self) -> str:
         return self.__message
+    @message.setter
+    def message(self, message: str) -> None:
+        self.__message = message
