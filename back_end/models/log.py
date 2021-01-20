60fdba21617355599586af1f4d0d953e5513cae6
josue-avila <josue.avila@olist.com>
2021-01-20 08:47:23 -0300

    using base modules to log_dao and log_model

--- back_end/models/log.py
+++ back_end/models/log.py
@@ -1,45 +1,21 @@
 from datetime import datetime
+from back_end.models.base_model import BaseModel
+from sqlalchemy import String, Column
 
-class Log:
-    __identifier: int
-    __hour: str
-    __date: str
-    __message: str
 
-    def __init__(self, message: str, hour: str = None, date: str = None, identifier: int = None):
-        if hour == None:
-            self.hour = datetime.now().strftime('%H:%M:%S')
-            self.date = datetime.now().strftime('%d:%m:%Y')
+class Log(BaseModel):
+    __tablename__ = 'log'
+
+    hora = Column(String(length=50))
+    data = Column(String(length=50))
+    mensagem = Column(String(length=200))
+
+    def __init__(self, mensagem: str, hora: str = None, data: str = None):
+        if hora == None:
+            self.hora = datetime.now().strftime('%H:%M:%S')
+            self.data = datetime.now().strftime('%d/%m/%Y')
         else:
-            self.hour = hour
-            self.date = date
-        self.message = message
-        self.identifier = identifier
-    
-    @property
-    def identifier(self) -> int:
-        return self.__identifier
-    @identifier.setter
-    def identifier(self, identifier: str) -> None:
-        self.__identifier = identifier
-    
-    @property
-    def hour(self) -> str:
-        return self.__hour
-    @hour.setter
-    def hour(self, hour: str) -> None:
-        self.__hour = hour
-    
-    @property
-    def date(self) -> str:
-        return self.__date
-    @date.setter
-    def date(self, date: str) -> None:
-        self.__date = date
-    
-    @property
-    def message(self) -> str:
-        return self.__message
-    @message.setter
-    def message(self, message: str) -> None:
-        self.__message = message
+            self.hora = hora
+            self.data = data
+        self.mensagem = mensagem
+
